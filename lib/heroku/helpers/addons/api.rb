dd0d655306e11136280e187bdde4a79167970769
Matthew Conway <himself@mattonrails.com>
2015-05-12 00:45:28 -0700

    Restore display of add-on plan price on provision

--- lib/heroku/helpers/addons/api.rb
+++ lib/heroku/helpers/addons/api.rb
@@ -74,6 +74,11 @@ module Heroku::Helpers
         request_list(:path => path)
       end
 
+      def get_plan(options = {})
+        path = "/addon-services/#{options[:service]}/plans/#{options[:plan]}"
+        request(:path => path)
+      end
+
       private
 
       def addons_path(options)
